369295a213df68180f29053a893a973cedfdb08d
Minwicorn <149855396+Minwicorn@users.noreply.github.com>
2025-11-17 05:42:45 +0800

    feat: create button functionality and page

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.engine('hbs', handlebars.engine);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api/blogposts', blogRoutes);
 
 /******************DATABASE CONNECTION************************/
@@ -163,6 +164,51 @@ app.get('/blog', async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(500).send('Server error');
+  }
+}); 
+
+/*
+ * 1. THE 'GET' ROUTE
+ * This route's only job is to SHOW your create page.
+ */
+app.get('/blog/create', (req, res) => {
+  res.render('create_modal', { // Or 'create.hbs' if you renamed it
+    title: "Create New Post" 
+  });
+});
+
+/*
+ * 2. THE 'POST' ROUTE
+ * This route's only job is to HANDLE the form data.
+ */
+app.post('/blog/create', async (req, res) => {
+  try {
+    // 1. Get the data from the form
+    const { title, content, tags } = req.body;
+    
+    // 2. Process the tags
+    const tagsArray = tags
+      .split(',')
+      .map(tag => tag.trim())
+      .filter(tag => tag.length > 0);
+
+    // 3. Save to database (or console.log for now)
+    console.log("New Post Received:");
+    console.log("Title:", title);
+    console.log("Content:", content);
+    console.log("Tags:", tagsArray);
+    
+    // 4. Send ONE response: Redirect the user to the blog list
+    res.redirect('/blog');
+    
+    // ----------------------------------------------------
+    // DO NOT put res.render() here.
+    // A request can only have one response.
+    // ----------------------------------------------------
+    
+  } catch (error) {
+    console.error("Error creating post:", error);
+    res.status(500).send("Error creating post.");
   }
 });
 
